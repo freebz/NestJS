bfdc58316bdf9b8065a2e5297506d3839a378a5f
freebx <freebx@gmail.com>
2024-08-04 11:30:28 +0900

    CHAPTER 3 애플리케이션 관문: 인터페이스

---
+++ ch03/user-service/src/users/users.controller.ts
@@ -0,0 +1,72 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete,
+  Res, HttpCode, BadRequestException, Header, Redirect, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   return this.usersService.create(createUserDto);
+  // }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    const { name, email } = createUserDto;
+
+    return `유저를 생성했습니다. 이름: ${name}, 이메일: ${email}`;
+  }
+
+  @Get()
+  findAll(@Res() res) {
+    const users = this.usersService.findAll();
+
+    return res.status(200).send(users);
+  }
+
+  @Get(':id')
+  @Header('Custom', 'Test Header')
+  @Redirect('https://nestjs.com', 301)
+  findOne(@Param('id') id: string) {
+    if (+id < 1) {
+      throw new BadRequestException('id는 0보다 큰 값이어야 합니다.');
+    }
+
+    return this.usersService.findOne(+id);
+  }
+
+  @HttpCode(202)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+
+  @Get('redirect/docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+
+  // @Delete(':userId/memo/:memoId')
+  // deleteUserMemo(@Param() params: { [key: string]: string }) {
+  //   return `userId: ${params.userId}, memoId: ${params.memoId}`;
+  // }
+
+  @Delete(':userId/memo/:memoId')
+  deleteUserMemo(
+    @Param('userId') userId: string,
+    @Param('memoId') memoId: string,
+  ) {
+    return `userId: ${userId}, memoId: ${memoId}`;
+  }
+}
